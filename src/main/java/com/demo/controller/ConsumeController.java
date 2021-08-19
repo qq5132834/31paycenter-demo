78a2451965d0e6d215eb579b87ccdb11aed7fa52
DESKTOP-6P9EJ20\51328 <513283439@qq.com>
2021-08-19 14:34:05 +0800

    ok

--- src/main/java/com/demo/controller/ConsumeController.java
+++ src/main/java/com/demo/controller/ConsumeController.java
@@ -1,16 +1,13 @@
 package com.demo.controller;
 
-import com.sany.truck.pub.pay.account.OpenAccountSendMsgRequest;
-import com.sany.truck.pub.pay.account.OpenAccountService;
-import com.sany.truck.pub.pay.consume.PayCenterConsumeRequest;
+import com.sany.truck.pub.pay.consume.PayCenterConsumePrePayRequest;
 import com.sany.truck.pub.pay.consume.PayCenterConsumeService;
+import com.sany.truck.pub.pay.consume.PayCenterConsumeToPayRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/consume")
@@ -24,15 +21,23 @@ public class ConsumeController {
     @ResponseBody
     @GetMapping("/prepay")
     public String prepay() throws Exception {
-        PayCenterConsumeRequest request = new PayCenterConsumeRequest();
+        PayCenterConsumePrePayRequest request = new PayCenterConsumePrePayRequest();
         request.setMerchantCode("A001");
         request.setAmount(100l);
         request.setConsumeOrderId("ORDERID112358");
         request.setConsumeOrderName("订单名称");
         request.setTimestamp(System.currentTimeMillis());
+        request.setCustomerAccontNo("1007001002004");
         return this.payCenterConsumeService.consumePrepay(request);
     }
 
-
+    @ApiOperation("结算宝支付")
+    @ResponseBody
+    @PostMapping("/topay")
+    public String topay(@RequestBody PayCenterConsumeToPayRequest request) throws Exception {
+        request.setPayMethod(2); //1密码支付，2免密支付
+        request.setPayAccountNo("123456"); //支付账户
+        return this.payCenterConsumeService.consumeTopay(request);
+    }
 
 }
